8c0b069253bd451a6cb16b359dfdae2ab4c8406e
LanceSolf <lancesolf@hotmail.com>
2026-01-11 21:11:33 +0100

    chore: remove obsolete tool scripts

--- tools/test-fetch.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-const url = 'https://static.lawinen-warnung.eu/bulletins/2026-01-09/2026-01-09_DE-BY_en_CAAMLv6.json';
-console.log('Testing ' + url);
-https.get(url, (res) => {
-    console.log('Status:', res.statusCode);
-});
